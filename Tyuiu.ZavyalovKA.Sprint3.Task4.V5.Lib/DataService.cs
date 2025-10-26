541b53dad7b60d180a4fc0ee9481d1d40a171015
Константин Завьялов <Tyuiu.ZavyalovKA@outlook.com>
2025-10-26 13:54:23 +0500

    Task4

--- Tyuiu.ZavyalovKA.Sprint3.Task4.V5.Lib/DataService.cs
+++ Tyuiu.ZavyalovKA.Sprint3.Task4.V5.Lib/DataService.cs
@@ -3,23 +3,21 @@ namespace Tyuiu.ZavyalovKA.Sprint3.Task4.V5.Lib
 {
     public class DataService : ISprint3Task4V5
     {
-        double ISprint3Task4V5.Calculate(int startValue, int stopValue)
+        public double Calculate(int startValue, int stopValue)
         {
+            double res = 0;
+            for (int i = startValue; i <= stopValue; i++)
             {
-                double res = 0;
-                for (int i = startValue; i <= stopValue; i++)
+                if (i == 0)
                 {
-                    if (i == 0)
-                    {
-                        break;
-                    }
-                    else
-                    {
-                        res = res + (Math.Cos(i) - Math.Sin(i)) / i;
-                    }
+                    break;
                 }
-                return Math.Round(res, 3);
+                else
+                {
+                    res = res + (Math.Cos(i) - Math.Sin(i)) / i;
+                }
             }
+            return Math.Round(res, 3);
         }
     }
 }
--- Tyuiu.ZavyalovKA.Sprint3.Task4.V5.Test/DataServiceTest.cs
+++ Tyuiu.ZavyalovKA.Sprint3.Task4.V5.Test/DataServiceTest.cs
@@ -1,4 +1,5 @@
-﻿using Tyuiu.ZavyalovKA.Sprint3.Task4.V5.Lib;
+﻿
+using Tyuiu.ZavyalovKA.Sprint3.Task4.V5.Lib;
 namespace Tyuiu.ZavyalovKA.Sprint3.Task4.V5.Test
 {
     [TestClass]
@@ -11,8 +12,9 @@ namespace Tyuiu.ZavyalovKA.Sprint3.Task4.V5.Test
             int startValue = -5;
             int stopValue = 5;
             var res = ds.Calculate(startValue, stopValue);
-            double wait = 0.104;
+            double wait = -0.858;
             Assert.AreEqual(wait, res);
         }
     }
 }
+
