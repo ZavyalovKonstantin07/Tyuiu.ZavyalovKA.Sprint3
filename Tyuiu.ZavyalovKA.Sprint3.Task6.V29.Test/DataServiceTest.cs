00b942393eae3e30ee48bc97db63267f75fb0ee1
Константин Завьялов <Tyuiu.ZavyalovKA@outlook.com>
2025-10-26 16:55:15 +0500

    Task6

---
+++ Tyuiu.ZavyalovKA.Sprint3.Task6.V29.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.ZavyalovKA.Sprint3.Task6.V29.Lib;
+namespace Tyuiu.ZavyalovKA.Sprint3.Task6.V29.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumTheDivisors()
+        {
+            DataService ds = new DataService();
+            int startValue = 12;
+            int stopValue = 18;
+            var res = ds.GetSumTheDivisors(startValue, stopValue);
+            int wait = 178;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ZavyalovKA.Sprint3.Task6.V29/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tyuiu.ZavyalovKA.Sprint3.Task6.V29.Lib;
+DataService ds = new DataService();
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+Console.WriteLine("***************************************************************************");
+int startValue = 12;
+int stopValue = 18;
+Console.WriteLine("Начало отрезка = " + startValue);
+Console.WriteLine("Конец отрезка = " + stopValue);
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("Сумма делителей = " + ds.GetSumTheDivisors(startValue, stopValue));
+Console.ReadKey();
