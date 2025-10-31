8600acb8b6baf37160a54fca8cb9c5aea9fb8c82
Константин Завьялов <Tyuiu.ZavyalovKA@outlook.com>
2025-10-31 22:26:35 +0500

    Task7

---
+++ Tyuiu.ZavyalovKA.Sprint3.Task7.V27/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tyuiu.ZavyalovKA.Sprint3.Task7.V27.Lib;
+DataService ds = new DataService();
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+Console.WriteLine("***************************************************************************");
+
+int startValue = -5;
+int stopValue = 5;
+
+Console.WriteLine("Старт шага = " + startValue);
+Console.WriteLine("Конец шага = " + stopValue);
+
+int len = ds.GetMassFunction(startValue, stopValue).Length;
+
+double[] valueArray;
+valueArray = new double[len];
+
+valueArray = ds.GetMassFunction(startValue, stopValue);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("+----------+----------+");
+Console.WriteLine("|    X     |   F(X)   |");
+Console.WriteLine("+----------+----------+");
+for (int i = 0; i <= len - 1; i++)
+{
+    Console.WriteLine("|{0,5}     | {1,7:f2}  |", startValue, valueArray[i]);
+    startValue++;
+}
+Console.WriteLine("+----------+----------+");
+Console.ReadKey();
