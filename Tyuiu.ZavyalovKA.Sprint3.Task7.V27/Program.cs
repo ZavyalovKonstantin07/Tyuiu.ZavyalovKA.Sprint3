77cd20374860d33863396ca0c20f966a0972930d
Константин Завьялов <Tyuiu.ZavyalovKA@outlook.com>
2025-11-01 11:58:15 +0500

    Task7

--- Tyuiu.ZavyalovKA.Sprint3.Task7.V27/Program.cs
+++ Tyuiu.ZavyalovKA.Sprint3.Task7.V27/Program.cs
@@ -1,35 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Tyuiu.ZavyalovKA.Sprint3.Task7.V27.Lib;
-            DataService ds = new DataService();
+DataService ds = new DataService();
 
-            Console.WriteLine("******************************");
-            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:          *");
-            Console.WriteLine("******************************");
+Console.WriteLine("******************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:          *");
+Console.WriteLine("******************************");
 
-            int startValue = -5;
-            int stopValue = 5;
+int startValue = -5;
+int stopValue = 5;
 
-            Console.WriteLine("Старт шага = " + startValue);
-            Console.WriteLine("Конец шага = " + stopValue);
+Console.WriteLine("Старт шага = " + startValue);
+Console.WriteLine("Конец шага = " + stopValue);
 
-            double[] valueArray = ds.GetMassFunction(startValue, stopValue);
-            int len = valueArray.Length;
+double[] valueArray = ds.GetMassFunction(startValue, stopValue);
+int len = valueArray.Length;
 
-            Console.WriteLine("******************************");
-            Console.WriteLine("* РЕЗУЛЬТАТ:                *");
-            Console.WriteLine("******************************");
+Console.WriteLine("******************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                *");
+Console.WriteLine("******************************");
 
-            Console.WriteLine("+----------+----------+");
-            Console.WriteLine("|    X     |   F(X)   |");
-            Console.WriteLine("+----------+----------+");
+Console.WriteLine("+----------+----------+");
+Console.WriteLine("|    X     |   F(X)   |");
+Console.WriteLine("+----------+----------+");
 
-            for (int i = 0; i < len; i++)
-            {
-                Console.WriteLine("|{0,5}     | {1,7:f2}  |", startValue, valueArray[i]);
-            }
-
-            Console.WriteLine("+----------+----------+");
-            Console.ReadKey();
-        }
-    }
+for (int i = 0; i < len; i++)
+{
+    Console.WriteLine("|{0,5:d}     | {1,7:f2}  |", startValue + i, valueArray[i]);
 }
+
+Console.WriteLine("+----------+----------+");
+Console.ReadKey();
+       
+    
+
